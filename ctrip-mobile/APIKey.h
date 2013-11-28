4a9e05b66d19892a600c723212f38c676babe223
guangyao <cgy8888@gmail.com>
2013-11-28 20:19:55 +0800

    add autonavi map kit apikey

---
+++ ctrip-mobile/APIKey.h
@@ -0,0 +1,14 @@
+//
+//  APIKey.h
+//  ctrip-mobile
+//
+//  Created by caoguangyao on 13-11-28.
+//  Copyright (c) 2013年 caoguangyao. All rights reserved.
+//
+
+#ifndef ctrip_mobile_APIKey_h
+#define ctrip_mobile_APIKey_h
+
+const static NSString *APIKey = @"9f8a18a57b2d19ada61b450fc0400028";
+
+#endif
